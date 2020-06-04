977028d5c878704051c0e42a8f6b31f5db4a409a
raphaeluziel <raphaeluziel@hotmail.com>
2020-06-04 17:12:20 -0400

    some styling issues resolved

--- recommendations/recommendations/settings.py
+++ recommendations/recommendations/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.raphaeluziel.net', '68.66.193.148']
+ALLOWED_HOSTS = []
 
 
 # Application definition
