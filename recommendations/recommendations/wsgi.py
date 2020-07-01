5c3c7d37776283010a0d202bd90fec417432030b
raphaeluziel <raphaeluziel@hotmail.com>
2020-07-01 11:13:38 -0400

    bbb

--- recommendations/recommendations/wsgi.py
+++ recommendations/recommendations/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import osg
 
 from django.core.wsgi import get_wsgi_application
 
